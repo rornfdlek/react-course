95c420f8592ec182fbc336289260010ed887b277
hnee <rornfdlek@naver.com>
2022-03-02 16:16:41 +0900

    48. More Context Usage

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,5 +1,22 @@
+import { useContext } from 'react';
+import MeetUpList from '../components/meetups/MeetUpList';
+import FavoritesContext from '../store/favorites-context';
+
 function FavoritesPage() {
-    return <div>Favorites Page</div>
+    const favoritesCtx = useContext(FavoritesContext);
+
+    let content;
+    if(favoritesCtx.totalFavorites ===0) {
+        content = <p>You got no favorites yet. Start adding some?</p>
+    }else {
+        content = <MeetUpList meetups={favoritesCtx.favorites}/>
+
+    }
+
+    return <section>
+        <h1>My Favorites</h1>
+        {content}
+    </section>
 }
 
 export default FavoritesPage;
